934c9afb658b1788239ee5bb612b7ee891640abf
Sergio Marin <higher.vnf@gmail.com>
2011-11-25 18:03:04 -0430

    Validation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '75023b909bab7170460af1d38649c87c6b48569a1e40a3902dd8cffc2417a970846ec0ca73b0846772971d4cfdf000150dc46a65ac39473c2a787211bc20ba47'
