6f07aefbe2b2177a5eba95120e3ded2a0b26926c
Sergio Marin <higher.vnf@gmail.com>
2011-11-25 18:27:59 -0430

    Store controller + bootstrap styles

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :image_url, :description, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, allow_blank: true, format: {
+  with: %r{\.(gif|jpg|png)$}i,
+  message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
