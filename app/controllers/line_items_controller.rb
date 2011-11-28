96ec1fbf1778b60b37331ecb2d64ec266147132c
Sergio Marin <higher.vnf@gmail.com>
2011-11-28 17:57:39 -0430

    Add Cart and Line Items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -40,12 +40,14 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(params[:line_item])
+    @cart = current_cart
+    product= Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.json { render json: @line_item, status: :created, location: @line_item }
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.json { render json: @line_item.cart, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
@@ -76,7 +78,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to @line_item.cart }
       format.json { head :ok }
     end
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,17 @@
 class Product < ActiveRecord::Base
   has_many :line_items
   
-  before_detroy
+  before_destroy
    :ensure_not_referenced_by_any_line_item
 
   private 
   def ensure_not_referenced_by_any_line_item
-	if line_items.empty?
-		return true
-	else
-		errors.add(:base, 'Line Items Present')
-		return false
-	end
+  	if line_items.empty?
+  		return true
+  	else
+  		errors.add(:base, 'Line Items Present')
+  		return false
+  	end
   end
   validates :title, :image_url, :description, presence: true
   validates :title, uniqueness: true
